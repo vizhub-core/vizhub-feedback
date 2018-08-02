8c3cd83ca95d030089758c8ceb080c789435e45a
Curran <curran.kelleher@gmail.com>
2018-08-03 01:35:33 +0530

    Fix delete confirm cancel case

--- src/redux/epics/confirmDeleteEpic.js
+++ src/redux/epics/confirmDeleteEpic.js
@@ -1,15 +1,14 @@
 import { from } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 import { DELETE_FILE } from '../actionTypes';
 import { fileDeleted } from '../actionCreators';
 
 export const confirmDeleteEpic = action$ =>
   action$.ofType(DELETE_FILE).pipe(
-    map(action => {
-      const confirmed = window.confirm('Are you sure?');
-      if (confirmed) {
-        return fileDeleted(action.fileName);
-      }
-      return null;
-    })
+    map(action => (
+      window.confirm('Are you sure?')
+        ? fileDeleted(action.fileName)
+        : null
+    )),
+    filter(Boolean)
   );
