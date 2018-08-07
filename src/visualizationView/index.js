98f16ffc7e6198b80abac4708e1467452d2e8a81
Curran <curran.kelleher@gmail.com>
2018-08-07 19:14:53 +0530

    Add styles for title

--- src/visualizationView/index.js
+++ src/visualizationView/index.js
@@ -1,19 +1,21 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { VisualizationRunner } from '../visualizationRunner/index.js';
 
 export const VisualizationView = props => {
   const { width, height, files, runId, title } = props;
   return (
-    <Fragment>
+    <div className='visualization-view'>
       <VisualizationRunner
         files={files}
         width={width}
         height={height}
         runId={runId}
       />
-      <div>
-        {title}
+      <div className='visualization-view-body'>
+        <div className='visualization-view-title'>
+          {title}
+        </div>
       </div>
-    </Fragment>
+    </div>
   );
 };
