1850edf0d2758d2f2b929ba467ee354f1d5c8f4a
curran <curran.kelleher@gmail.com>
2018-11-18 17:55:17 +0530

    Got horizontal centering to work in full screen mode, restoring viz view pane behavior

--- src/visualizationRunner/index.js
+++ src/visualizationRunner/index.js
@@ -18,10 +18,12 @@ export class VisualizationRunner extends Component {
     const boundsHeight = measureHeight ? this.state.bounds.height : Infinity;
     const scale = computeScale({ boundsWidth, boundsHeight, width, height });
     const srcDoc = this.getSrcDoc(files, runId);
+    const measureStyle = measureHeight ? { height: '100%' } : {};
+
     return (
       <Measure bounds onResize={this.onResize} >
         {({ measureRef }) =>
-          <div ref={measureRef} style={measureHeight ? { height: '100%' } : {}} >
+          <div ref={measureRef} style={measureStyle}>
             <div style={{
               width: `${width * scale}px`,
               height: `${height * scale}px`,
